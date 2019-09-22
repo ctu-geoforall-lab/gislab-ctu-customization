886371811cb8c960e66dcda0742524f65e03be4e
Martin Landa <landa.martin@gmail.com>
2019-09-22 16:59:05 +0200

    create users from CVS exported by KOS

---
+++ tools/create-users-from-kos.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+if test -z "$1" ; then
+    echo "usage: $0 path/to/csv"
+    exit 1
+fi
+
+process_csv() {
+SEP=','
+tail -n +3 $1 | while read line || [ -n "$line" ];
+do           
+    fname=`echo $line | cut -d $SEP -f2 | iconv -f utf-8 -t us-ascii//TRANSLIT`
+    sname=`echo $line | cut -d $SEP -f1 | iconv -f utf-8 -t us-ascii//TRANSLIT `
+    name=`echo $sname | iconv -f utf-8 -t us-ascii//TRANSLIT | tr '[:upper:]' '[:lower:]'`
+    passwd=`echo $fname | iconv -f utf-8 -t us-ascii//TRANSLIT | tr '[:upper:]' '[:lower:]'`
+    mail="$name@gislab.fsv.cvut.cz"
+
+    sudo gislab-deluser -f $name
+    sudo gislab-adduser -g $fname -l $sname -m $mail -a $2 -p ${passwd} $name
+done
+}
+
+filepath="$1"
+filename=`basename $filepath`
+group=${filename%%.csv}
+
+sudo gislab-addgroup $group
+process_csv $filepath $group
+
+sudo gislab-deluser -f landa
+sudo gislab-adduser -g Martin -l Landa -m landa@gislab.fsv.cvut.cz -a $group -p martin landa
+
+exit 0
